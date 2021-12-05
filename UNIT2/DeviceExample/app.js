9a36ffcd0fdcce34c88f3add63ec63d52a8aa220
FranciscoHerediaC <2009067@upy.edu.mx>
2021-12-05 09:01:13 -0600

    third

---
+++ UNIT2/DeviceExample/app.js
@@ -0,0 +1,17 @@
+//import {chalk} from "chalk"
+
+class Device {
+    constructor(Name, model, noLeds, location){
+        this.name = Name;
+        this.model = model;
+        this.noLeds = noLeds;
+        this.location = location;
+    };
+    turnOnLed(){
+        console.log('Led turned On...');
+    }
+};
+
+//instanciar Objecto
+const cesar = new Device("MyFirstDevice", "RaspberryPi4", 2, 1000);
+cesar.turnOnLed();
